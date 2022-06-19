b000d8925eebeeb11bd154a7c8c216417f1904e0
gwonhyeong.tak <gwonhyeong.tak@gmail.com>
2022-06-19 23:53:59 +0900

    [title] init repo

---
+++ tcpp/include/tcpp/tcpp.hpp
@@ -0,0 +1,8 @@
+#ifndef __tcpp_h__
+#define __tcpp_h__
+
+namespace tcpp{
+    int hellotcpp(void);
+}
+
+#endif
---
+++ tcpp/src/tcpp.cpp
@@ -0,0 +1,7 @@
+#include <tcpp/tcpp.hpp>
+
+namespace tcpp {
+    int hellotcpp(void){
+        return 81;
+    }
+}
