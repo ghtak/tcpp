9d92bb6e3cd3f152cad45426abe76b647406edea
gwonhyeong.tak <gwonhyeong.tak@gmail.com>
2022-06-21 23:30:11 +0900

    [title] add c++20 feature test

--- test/main.cpp
+++ test/main.cpp
@@ -2,7 +2,8 @@
 #include "catch2/catch.hh"
 
 #include <tcpp/tcpp.hpp>
-
+#include <ranges>
+#include <iostream>
 
 unsigned int factorial( unsigned int number ) {
     return number <= 1 ? number : factorial(number-1)*number;
@@ -14,4 +15,22 @@ TEST_CASE( "Factorials are computed", "[factorial]" ) {
     REQUIRE( factorial(3) == 6 );
     REQUIRE( factorial(10) == 3628800 );
     REQUIRE( tcpp::hellotcpp() == 81 );
+}
+
+TEST_CASE( "range", "[c++20]" ) {
+    auto const ints = {0,1,2,3,4,5};
+    auto even = [](int i) { return 0 == i % 2; };
+    auto square = [](int i) { return i * i; };
+ 
+    // "pipe" syntax of composing the views:
+    for (int i : ints | std::views::filter(even) | std::views::transform(square)) {
+        std::cout << i << ' ';
+    }
+ 
+    std::cout << '\n';
+ 
+    // a traditional "functional" composing syntax:
+    for (int i : std::views::transform(std::views::filter(ints, even), square)) {
+        std::cout << i << ' ';
+    }
 }
